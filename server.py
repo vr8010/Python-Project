6d69627c5552fb04d8e45855647d1aaa9e91cd89
Vishal rathod <121178282+vr8010@users.noreply.github.com>
2026-05-07 22:36:27 +0530

    ...

--- server.py
+++ server.py
@@ -8,11 +8,9 @@ from report_manager import ReportManager
 app = Flask(__name__, template_folder='frontend', static_folder='frontend/static')
 report_mgr = ReportManager()
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/api/analyze', methods=['POST'])
 def analyze():
@@ -24,11 +22,9 @@ def analyze():
     report_mgr.save_report(analysis)
     return jsonify(analysis)
 
-
 @app.route('/api/reports', methods=['GET'])
 def get_reports():
     return jsonify(report_mgr.get_reports_list())
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
